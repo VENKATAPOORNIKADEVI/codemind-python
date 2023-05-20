06fa79346ef936c9c6d913053233f8f386179f0b
VENKATAPOORNIKADEVI <121076147+VENKATAPOORNIKADEVI@users.noreply.github.com>
2023-05-20 14:30:08 +0530

    b=int(input()) c=b t=0 while b!=0:     v=b%10     t=t*10+v     b=b//10 if c==t: print("True") else:print("False")

--- Palindrome.py
+++ Palindrome.py
@@ -1,10 +1,9 @@
 b=int(input()) 
-c=b
+c=b 
 t=0
-while b!=0: 
-    v=b%10
-    t=t*10+v 
+while b!=0:
+    v=b%10 
+    t=t*10+v
     b=b//10 
-if c==t: 
-    print("True") 
-else:print("False") 
+if c==t: print("True") 
+else:print("False")
