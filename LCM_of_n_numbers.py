7ef8dd7268bc4eb7c09a46c25fd9e2641040fad1
VENKATAPOORNIKADEVI <121076147+VENKATAPOORNIKADEVI@users.noreply.github.com>
2023-06-01 19:25:49 +0530

    def l(a,b):     c=max(a,b)     while True:         if c%a==0 and c%b==0:             return c         c+=1 n=int(input()) a=list(map(int,input().split())) lcm=l(a[0],a[1]) for i in range(2,n):     lcm=l(lcm,a[i]) print(lcm)

---
+++ LCM_of_n_numbers.py
@@ -0,0 +1,12 @@
+def l(a,b):
+    c=max(a,b)
+    while True:
+        if c%a==0 and c%b==0:
+            return c
+        c+=1
+n=int(input())
+a=list(map(int,input().split()))
+lcm=l(a[0],a[1])
+for i in range(2,n):
+    lcm=l(lcm,a[i])
+print(lcm)
